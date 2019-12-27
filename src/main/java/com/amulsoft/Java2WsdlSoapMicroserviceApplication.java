6b468da837fdd69b4c62f5a72dfe2bc735221826
vinithamani309 <amul.manikanta143@gmail.com>
2019-12-27 21:59:20 +0530

    First Commit

---
+++ src/main/java/com/amulsoft/Java2WsdlSoapMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.amulsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2WsdlSoapMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java2WsdlSoapMicroserviceApplication.class, args);
+	}
+
+}
